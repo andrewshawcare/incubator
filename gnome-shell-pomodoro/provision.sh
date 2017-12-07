84337ca723afe6f187bd459aa8a76a615d2ff81d
Andrew Shaw Care <andrewshawcare@gmail.com>
2017-12-06 20:21:01 -0500

    Replace safeeyes with gnome-shell-pomodoro

---
+++ gnome-shell-pomodoro/provision.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt install --yes gnome-shell-pomodoro
--- safeeyes/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo add-apt-repository --yes --update ppa:slgobinath/safeeyes
-sudo apt install safeeyes
-
-touch ~/.Xauthority
