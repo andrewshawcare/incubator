bca09d1177c667f0f33edc58ba304d7978728f31
Andrew Shaw Care <andrewshawcare@gmail.com>
2018-01-10 19:00:47 -0500

    Remove iwlwifi

--- iwlwifi/provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-line='options iwlwifi 11n_disable=1'
-file='/etc/modprobe.d/iwlwifi.conf'
-
-grep "$line" "$file" || echo "$line" >> "$file"
--- iwlwifi/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-test "$(grep -o 'options iwlwifi 11n_disable=1' /etc/modprobe.d/iwlwifi.conf | wc -l)" -eq 1
